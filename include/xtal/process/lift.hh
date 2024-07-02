ad3667bf03489ac1bab0dc953a737beade36cc64
GoomTrex <goomtrex@gmail.com>
2024-07-02 10:19:06 +0930

    Expanding `lift` for `function`al mapping.

--- include/xtal/process/lift.hh
+++ include/xtal/process/lift.hh
@@ -13,32 +13,21 @@ namespace xtal::process
 
 template <typename ..._s> XTAL_TYP lift;
 template <typename ..._s> XTAL_USE lift_t = confined_t<lift<_s...>>;
-template <typename ..._s> XTAL_REQ lift_q = bond::any_tag_p<lift, _s...>;
 
 
-////////////////////////////////////////////////////////////////////////////////
-///\
-Provides pure `head`-less mapping of `method` and `function` (in contrast to `link`), \
-in addition to allowing constructor mapping via `invoke_f`. \
+namespace _detail
+{///////////////////////////////////////////////////////////////////////////////
 
-template <typename     ...As> struct lift    : bond::compose<lift<As>...> {};
-template <   incomplete_q A > struct lift<A> : bond::compose<           > {};
-template <bond::compose_q A > struct lift<A> :                          A {};
-template <class           F >
-struct lift<F>
+template <class F>
+struct lifter
 {
-	using subkind = bond::tag<lift>;
-
 	template <any_q S>
-	class subtype : public bond::compose_s<S, subkind>
+	class subtype : public bond::compose_s<S>
 	{
-		using S_ = bond::compose_s<S, subkind>;
-		using T_ = typename S_::self_type;
+		using S_ = bond::compose_s<S>;
 		
 	public:
 		using S_::S_;
-		using S_::self;
-		using S_::head;
 
 		template <auto ...Is>
 		XTAL_DEF_(return,inline)
@@ -55,6 +44,7 @@ struct lift<F>
 		{
 			return invoke_f<F>(S_::template method<Is...>(XTAL_REF_(xs)...));
 		}
+		
 		template <auto ...Is>
 		XTAL_DEF_(return,inline,static)
 		XTAL_LET function(auto &&...xs)
@@ -64,6 +54,57 @@ struct lift<F>
 
 	};
 };
+template <bond::compose_q F>
+struct lifter<F>
+{
+	template <any_q S>
+	class subtype : public bond::compose_s<S>
+	{
+		using S_ = bond::compose_s<S>;
+		using F_ = confined_t<F>;
+
+	public:
+		using S_::S_;
+
+		template <auto ...Is>
+		XTAL_DEF_(return,inline)
+		XTAL_LET method(auto &&...xs)
+		XTAL_0EX -> decltype(auto)
+		{
+			return F_::template function<Is...>(S_::template method<Is...>(XTAL_REF_(xs)...));
+		}
+		template <auto ...Is>
+		XTAL_DEF_(return,inline)
+		XTAL_LET method(auto &&...xs)
+		XTAL_0FX -> decltype(auto)
+			requires XTAL_TRY_(XTAL_ANY_(S_ const &).template method<Is...>(XTAL_REF_(xs)...))
+		{
+			return F_::template function<Is...>(S_::template method<Is...>(XTAL_REF_(xs)...));
+		}
+		
+		template <auto ...Is>
+		XTAL_DEF_(return,inline,static)
+		XTAL_LET function(auto &&...xs)
+		XTAL_0EX
+		->	decltype(auto)
+			requires XTAL_TRY_TO_(F_::template function<Is...>(S_::template function<Is...>(XTAL_REF_(xs)...)))
+
+	};
+};
+
+
+}///////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+///\
+Provides pure `head`-less mapping of `method` and `function` (in contrast to `link`), \
+in addition to allowing constructor mapping via `invoke_f`. \
+
+template <                   typename ...As> struct lift;
+template <bond::compose_q A                > struct lift<A       > :                                              A {};
+template <                                 > struct lift<        > : bond::compose<                               > {};
+template <     typename   A, typename ...As> struct lift<A, As...> : bond::compose<_detail::lifter<A>, lift<As...>> {};
+template <   incomplete_q A, typename ...As> struct lift<A, As...> :                                   lift<As...>  {};
 
 
 ////////////////////////////////////////////////////////////////////////////////
