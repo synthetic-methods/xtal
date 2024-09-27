1d017056a1782bab2c72718075badfa87b8d2dbd
GoomTrex <goomtrex@gmail.com>
2024-09-27 15:38:58 +0930

    Provisional one-shot `linear` ramping.

--- include/xtal/algebra/differential/linear.hh
+++ include/xtal/algebra/differential/linear.hh
@@ -54,6 +54,60 @@ struct linear<A>
 		using S_::self;
 		using S_::twin;
 
+		template <class Y=U_data>
+		XTAL_DEF_(inline)
+		XTAL_LET advice()
+		XTAL_0EX -> auto
+		{
+		//TODO: Should accumulate `advice` for all except the last element? Or like all?
+		//TODO: Handle downwards ramps too, so should just ensure they are all identical or whatever.
+			using _op = bond::operate<Y>;
+			using alpha_type = typename _op::alpha_type;
+			using sigma_type = typename _op::sigma_type;
+			using delta_type = typename _op::delta_type;
+
+			auto const sign_bit = get<0>(*this)&_op::sign.mask;
+			auto const sign_val = static_cast<Y>(sign_bit) >> _op::sign.shift;
+			XTAL_IF0
+			XTAL_0IF (       is_q<bool, Y>) {return !sign_bit;}
+			XTAL_0IF (integral_number_q<Y>) {return  sign_val;}
+			XTAL_0IF (    real_number_q<Y>) {return _xtd::bit_cast<alpha_type>(sign_bit|sign_val&_op::unit.mask);}
+		}
+		template <int N=1>
+		XTAL_DEF_(inline)
+		XTAL_LET advent()
+		XTAL_0EX -> auto
+		{
+			auto &s = self();
+			auto const n = s.advice(); s.template advance<N>();
+			auto const m = s.advice(); return m^n;
+		}
+		template <int N=1>
+		XTAL_DEF_(inline)
+		XTAL_LET advance()
+		XTAL_0EX -> T &
+		{
+			XTAL_IF0
+			XTAL_0IF (0 < N) {
+				return operator++();
+			}
+			XTAL_0IF (N < 0) {
+				return operator--();
+			}
+		}
+		template <int N=1>
+		XTAL_DEF_(inline)
+		XTAL_LET advanced()
+		XTAL_0EX -> T
+		{
+			XTAL_IF0
+			XTAL_0IF (0 < N) {
+				return operator++(0);
+			}
+			XTAL_0IF (N < 0) {
+				return operator--(0);
+			}
+		}
 		///\
 		Produces the successor by pairwise addition from `begin()` to `end()`, \
 		assuming the entries of `this` are finite differences/derivatives. \
@@ -71,7 +125,15 @@ struct linear<A>
 			auto &s = self();
 
 			[&]<auto ...I> (bond::seek_t<I...>)
-				XTAL_0FN {((get<I>(s) += get<I + 1>(s)),...);}
+			XTAL_0FN {
+				if constexpr (_std::is_signed_v<U_data>) {
+					((reinterpret_cast<_std::make_unsigned_t<U_data> &>(get<I>(s)) += get<I + 1>(s)^advice()),...);
+
+				}
+				else {
+					((get<I>(s) += get<I + 1>(s)),...);
+				}
+			}
 			(bond::seek_s<N_data - 1>{});
 			
 			return s;
@@ -94,7 +156,14 @@ struct linear<A>
 			auto &s = self();
 
 			[&]<auto ...I> (bond::seek_t<I...>)
-				XTAL_0FN {((get<I>(s) -= get<I + 1>(s)),...);}
+			XTAL_0FN {
+				if constexpr (_std::is_signed_v<U_data>) {
+					((reinterpret_cast<_std::make_unsigned_t<U_data> &>(get<I>(s)) -= get<I + 1>(s)^advice()),...);
+				}
+				else {
+					((get<I>(s) -= get<I + 1>(s)),...);
+				}
+			}
 			(bond::antiseek_s<N_data - 1>{});
 			
 			return s;
