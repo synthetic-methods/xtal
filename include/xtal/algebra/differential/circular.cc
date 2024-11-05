1a0f2c9fc28b10e37b0bd9f0177e371810762f9e
GoomTrex <goomtrex@gmail.com>
2024-11-05 23:30:22 +1030

    Adapting `circular` to `float` upgrade.

--- include/xtal/algebra/differential/circular.cc
+++ include/xtal/algebra/differential/circular.cc
@@ -15,7 +15,7 @@ namespace xtal::algebra::differential::_test
 
 TAG_("circular")
 {
-	using _op = bond::operating;
+	using _op = bond::operate<float>;
 	using T_sigma = typename _op::sigma_type;
 	using T_delta = typename _op::delta_type;
 	using T_alpha = typename _op::alpha_type;
@@ -24,7 +24,7 @@ TAG_("circular")
 	static constexpr T_alpha six =  6;
 	static constexpr T_alpha ten = 10;
 
-	auto mt19937_f = typename operating::mt19937_t(Catch::rngSeed());
+	auto mt19937_f = _op::mt19937_t(Catch::rngSeed());
 
 	using V_phi = T_alpha;
 	using U_phi = circular_t<V_phi[2]>;
@@ -32,10 +32,10 @@ TAG_("circular")
 	using M_phi = circular_t<_std::complex<T_alpha>[2]>;
 	using A_phi = _std::array<V_phi, 2>;
 
-	using D1 = circular_t<T_delta[1]>;
-	using D2 = circular_t<T_delta[2]>;
-	using D3 = circular_t<T_delta[3]>;
-	using D4 = circular_t<T_delta[4]>;
+	using D1 = circular_t<T_alpha[1]>;
+	using D2 = circular_t<T_alpha[2]>;
+	using D3 = circular_t<T_alpha[3]>;
+	using D4 = circular_t<T_alpha[4]>;
 	
 	TRY_("phasor of complex")
 	{
@@ -139,14 +139,13 @@ TAG_("circular")
 		U_phi phi{x, x_dt};
 		V_phi foo{x};
 
-		for (T_sigma i = 0; i < 0x8; ++i) {
-		//	T_alpha const u = -_std::pow(six, 1 + _op::mantissa_f(mt19937_f));
+		for (T_sigma i = 0; i < 0x4; ++i) {
 			auto const v = static_cast<T_alpha>(i + 1);
 			auto const u{exp(asinh(v*_op::haplo_1))};
 			phi *= u;
 			foo *= u;
 			foo -= _std::round(foo);
-			TRUE_(check_f<-31>(phi(0), foo));
+			TRUE_(check_f<8>(phi(0), foo));
 		}
 	}
 	/**/
